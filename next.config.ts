3d9d8fac9599839874200432f24d29f4202b406e
muntahaaa <bsse1450@iit.du.ac.bd>
2025-08-25 02:51:43 +0600

    removed unused files

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+    output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -9,14 +10,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+    unoptimized: true,
   },
 };
 
